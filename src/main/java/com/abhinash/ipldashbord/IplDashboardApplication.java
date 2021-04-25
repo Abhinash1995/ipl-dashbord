303795f9a7d957c141fe1d59468923ce46b9762b
Abhinash <Atanas.atansov@sunrise.net>
2021-04-25 19:01:51 +0530

    project setup and code for back processing

---
+++ src/main/java/com/abhinash/ipldashbord/IplDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.abhinash.ipldashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinash/ipldashbord/IplDashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinash.ipldashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
