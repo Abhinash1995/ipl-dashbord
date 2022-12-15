4684a9ffe0e679a2db405293e31fe1b4c4337ba9
Abhinash7643 <abhinash7643@gmail.com>
2022-12-15 23:19:00 +0530

    added api for get all teams and get team matches by year

--- src/main/java/com/abhinash/ipldashbord/controller/TeamController.java
+++ src/main/java/com/abhinash/ipldashbord/controller/TeamController.java
@@ -1,17 +1,22 @@
 package com.abhinash.ipldashbord.controller;
 
+import com.abhinash.ipldashbord.model.Match;
 import com.abhinash.ipldashbord.model.Team;
 import com.abhinash.ipldashbord.repository.MatchRepository;
 import com.abhinash.ipldashbord.repository.TeamRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class TeamController {
 
-    private TeamRepository teamRepository;
-    private MatchRepository matchRepository;
+    private final TeamRepository teamRepository;
+    private final MatchRepository matchRepository;
 
     public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
         this.teamRepository = teamRepository;
@@ -25,5 +30,21 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team")
+    public Iterable<Team> getAllTeam() {
+        return this.teamRepository.findAll();
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+                teamName,
+                startDate,
+                endDate
+        );
     }
 }
